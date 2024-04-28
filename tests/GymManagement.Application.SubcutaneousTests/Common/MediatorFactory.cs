9baf7baed2206d75bdc96ec6a0ccff9095259bbd
root <root@DESKTOP-AVS9I9I.>
2024-04-28 09:07:27 -0400

    formatting

--- tests/GymManagement.Application.SubcutaneousTests/Common/MediatorFactory.cs
+++ tests/GymManagement.Application.SubcutaneousTests/Common/MediatorFactory.cs
@@ -22,29 +22,34 @@ public class MediatorFactory : WebApplicationFactory<IAssemblyMarker>, IAsyncLif
     {
         _testDatabase = SqliteTestDatabase.CreateAndInitialize();
 
-                builder.ConfigureTestServices(services =>
-                {
-                    services
-                        .RemoveAll<DbContextOptions<GymManagementDbContext>>()
-                        .AddDbContext<GymManagementDbContext>((sp, options) => options.UseSqlite(_testDatabase.Connection));
+        builder.ConfigureTestServices(services =>
+        {
+            services
+                .RemoveAll<DbContextOptions<GymManagementDbContext>>()
+                .AddDbContext<GymManagementDbContext>(
+                    (sp, options) => options.UseSqlite(_testDatabase.Connection)
+                );
 
-                        var httpContextAccessorSubstitute = Substitute.For<IHttpContextAccessor>();
+            var httpContextAccessorSubstitute = Substitute.For<IHttpContextAccessor>();
 
-                        var user = new ClaimsPrincipal(new ClaimsIdentity(
-                            new List<Claim>
-                            {
-                                new Claim("id", Guid.NewGuid().ToString()),
-                                new Claim(ClaimTypes.Name, "TestUser"),
-                                new Claim(ClaimTypes.Role, "Admin")
-                            }, 
-                            "TestAuthenticationType"));
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new List<Claim>
+                    {
+                        new("id", Guid.NewGuid().ToString()),
+                        new(ClaimTypes.Name, "TestUser"),
+                        new(ClaimTypes.Role, "Admin")
+                    },
+                    "TestAuthenticationType"
+                )
+            );
 
-                        var httpContext = new DefaultHttpContext { User = user };
+            var httpContext = new DefaultHttpContext { User = user };
 
-                        httpContextAccessorSubstitute.HttpContext.Returns(httpContext);
+            httpContextAccessorSubstitute.HttpContext.Returns(httpContext);
 
-                        services.AddSingleton(httpContextAccessorSubstitute);
-                });
+            services.AddSingleton(httpContextAccessorSubstitute);
+        });
     }
 
     public IMediator CreateMediator()
@@ -55,7 +60,6 @@ public class MediatorFactory : WebApplicationFactory<IAssemblyMarker>, IAsyncLif
 
         return serviceScope.ServiceProvider.GetRequiredService<IMediator>();
     }
-
 
     public Task InitializeAsync() => Task.CompletedTask;
 
